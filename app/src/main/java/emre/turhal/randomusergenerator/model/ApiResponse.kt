52a8693c8dfd57f65c1c45c6dc971b3d26e2e3c2
Emre Turhal <emreturhal9@gmail.com>
2023-06-28 01:41:52 +0200

    converted JSON TO POJO with plugin

---
+++ app/src/main/java/emre/turhal/randomusergenerator/model/ApiResponse.kt
@@ -0,0 +1,91 @@
+package emre.turhal.randomusergenerator.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponse(
+
+	@field:SerializedName("results")
+	val results: List<ResultsItem?>? = null,
+
+	@field:SerializedName("info")
+	val info: Info? = null
+)
+
+data class Location(
+
+	@field:SerializedName("country")
+	val country: String? = null,
+
+	@field:SerializedName("city")
+	val city: String? = null,
+
+	@field:SerializedName("postcode")
+	val postcode: Int? = null,
+
+	@field:SerializedName("coordinates")
+	val coordinates: Coordinates? = null,
+
+	@field:SerializedName("state")
+	val state: String? = null
+)
+
+data class Info(
+
+	@field:SerializedName("seed")
+	val seed: String? = null,
+
+	@field:SerializedName("page")
+	val page: Int? = null,
+
+	@field:SerializedName("results")
+	val results: Int? = null,
+
+	@field:SerializedName("version")
+	val version: String? = null
+)
+
+data class Coordinates(
+
+	@field:SerializedName("latitude")
+	val latitude: String? = null,
+
+	@field:SerializedName("longitude")
+	val longitude: String? = null
+)
+
+data class Name(
+
+	@field:SerializedName("last")
+	val last: String? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("first")
+	val first: String? = null
+)
+
+data class ResultsItem(
+
+	@field:SerializedName("name")
+	val name: Name? = null,
+
+	@field:SerializedName("location")
+	val location: Location? = null,
+
+	@field:SerializedName("picture")
+	val picture: Picture? = null
+)
+
+
+data class Picture(
+
+	@field:SerializedName("thumbnail")
+	val thumbnail: String? = null,
+
+	@field:SerializedName("large")
+	val large: String? = null,
+
+	@field:SerializedName("medium")
+	val medium: String? = null
+)
