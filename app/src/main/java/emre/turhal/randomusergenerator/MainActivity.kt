444d615445eeab45b2a468ad5e70a0a098a6a292
Emre Turhal <emreturhal9@gmail.com>
2023-06-27 13:13:48 +0200

    Initial commit

---
+++ app/src/main/java/emre/turhal/randomusergenerator/MainActivity.kt
@@ -0,0 +1,11 @@
+package emre.turhal.randomusergenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
