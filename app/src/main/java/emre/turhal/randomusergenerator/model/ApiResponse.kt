16edbcd516e10bd2c2bcfbdd14a13498cc0a8026
Emre Turhal <emreturhal9@gmail.com>
2023-07-18 17:31:16 +0200

    changed pojo model to retrieve extras infos

--- app/src/main/java/emre/turhal/randomusergenerator/model/ApiResponse.kt
+++ app/src/main/java/emre/turhal/randomusergenerator/model/ApiResponse.kt
@@ -7,6 +7,7 @@ import java.io.Serializable
 //Model POJO, CONVERTED JSON
 data class ApiResponse(
 
+
 	@field:SerializedName("results")
 	val results: List<ResultsItem?>? = null,
 
@@ -14,19 +15,55 @@ data class ApiResponse(
 	val info: Info? = null
 ):Serializable
 
-data class Location(
+data class Name(
 
-	@field:SerializedName("country")
-	val country: String? = null,
+	@field:SerializedName("last")
+	val last: String? = null,
 
-	@field:SerializedName("city")
-	val city: String? = null,
+	@field:SerializedName("title")
+	val title: String? = null,
 
-	@field:SerializedName("coordinates")
-	val coordinates: Coordinates? = null,
+	@field:SerializedName("first")
+	val first: String? = null
+):Serializable
 
-	@field:SerializedName("state")
-	val state: String? = null
+data class Id(
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("value")
+	val value: String? = null
+):Serializable
+
+data class Coordinates(
+
+	@field:SerializedName("latitude")
+	val latitude: String? = null,
+
+	@field:SerializedName("longitude")
+	val longitude: String? = null
+):Serializable
+
+data class Timezone(
+
+	@field:SerializedName("offset")
+	val offset: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null
+):Serializable
+
+data class Picture(
+
+	@field:SerializedName("thumbnail")
+	val thumbnail: String? = null,
+
+	@field:SerializedName("large")
+	val large: String? = null,
+
+	@field:SerializedName("medium")
+	val medium: String? = null
 ):Serializable
 
 data class Info(
@@ -42,50 +79,119 @@ data class Info(
 
 	@field:SerializedName("version")
 	val version: String? = null
+): Serializable
+
+data class Registered(
+
+	@field:SerializedName("date")
+	val date: String? = null,
+
+	@field:SerializedName("age")
+	val age: Int? = null
+): Serializable
+
+data class Location(
+
+	@field:SerializedName("country")
+	val country: String? = null,
+
+	@field:SerializedName("city")
+	val city: String? = null,
+
+	@field:SerializedName("street")
+	val street: Street? = null,
+
+	@field:SerializedName("timezone")
+	val timezone: Timezone? = null,
+
+	@field:SerializedName("postcode")
+	val postcode: String? = null,
+
+	@field:SerializedName("coordinates")
+	val coordinates: Coordinates? = null,
+
+	@field:SerializedName("state")
+	val state: String? = null
+): Serializable
+
+data class Street(
+
+	@field:SerializedName("number")
+	val number: Int? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null
+): Serializable
+
+data class Dob(
+
+	@field:SerializedName("date")
+	val date: String? = null,
+
+	@field:SerializedName("age")
+	val age: Int? = null
 ):Serializable
 
-data class Coordinates(
+data class Login(
 
-	@field:SerializedName("latitude")
-	val latitude: String? = null,
+	@field:SerializedName("sha1")
+	val sha1: String? = null,
 
-	@field:SerializedName("longitude")
-	val longitude: String? = null
-):Serializable
+	@field:SerializedName("password")
+	val password: String? = null,
 
-data class Name(
+	@field:SerializedName("salt")
+	val salt: String? = null,
 
-	@field:SerializedName("last")
-	val last: String? = null,
+	@field:SerializedName("sha256")
+	val sha256: String? = null,
 
-	@field:SerializedName("title")
-	val title: String? = null,
+	@field:SerializedName("uuid")
+	val uuid: String? = null,
 
-	@field:SerializedName("first")
-	val first: String? = null
+	@field:SerializedName("username")
+	val username: String? = null,
+
+	@field:SerializedName("md5")
+	val md5: String? = null
 ):Serializable
 
 data class ResultsItem(
 
+	@field:SerializedName("nat")
+	val nat: String? = null,
+
+	@field:SerializedName("gender")
+	val gender: String? = null,
+
+	@field:SerializedName("phone")
+	val phone: String? = null,
+
+	@field:SerializedName("dob")
+	val dob: Dob? = null,
+
 	@field:SerializedName("name")
 	val name: Name? = null,
+
+	@field:SerializedName("registered")
+	val registered: Registered? = null,
 
 	@field:SerializedName("location")
 	val location: Location? = null,
 
+	@field:SerializedName("id")
+	val id: Id? = null,
+
+	@field:SerializedName("login")
+	val login: Login? = null,
+
+	@field:SerializedName("cell")
+	val cell: String? = null,
+
+	@field:SerializedName("email")
+	val email: String? = null,
+
 	@field:SerializedName("picture")
 	val picture: Picture? = null
-):Serializable
+): Serializable
 
-
-data class Picture(
-
-	@field:SerializedName("thumbnail")
-	val thumbnail: String? = null,
-
-	@field:SerializedName("large")
-	val large: String? = null,
-
-	@field:SerializedName("medium")
-	val medium: String? = null
-):Serializable
