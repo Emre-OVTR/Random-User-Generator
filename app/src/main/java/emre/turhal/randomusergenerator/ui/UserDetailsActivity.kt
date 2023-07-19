eab64bf2e03999feb64339f31e68579c14843d23
Emre Turhal <emreturhal9@gmail.com>
2023-07-19 18:36:22 +0200

    fetched data from api  to retrieve users details in new activityuserdetails

--- app/src/main/java/emre/turhal/randomusergenerator/ui/UserDetailsActivity.kt
+++ app/src/main/java/emre/turhal/randomusergenerator/ui/UserDetailsActivity.kt
@@ -1,6 +1,7 @@
 package emre.turhal.randomusergenerator.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -19,21 +20,38 @@ class UserDetailsActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         displayUser()
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "User Details"
+
     }
 
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
     companion object{
         const val USER = "USER"
     }
 
     private fun displayUser(){
         val imageView = binding.userPhoto
-        val name = binding.name
-        val dob = binding.dob
         user = intent.getSerializableExtra(USER) as ResultsItem
         Glide.with(this).load(user.picture?.large)
             .apply(RequestOptions().circleCrop()).into(imageView)
 
-       name.text = "${user.name?.first} ${user.name?.last}"
+        binding.name.text = "${user.name?.first} ${user.name?.last}"
+        binding.dob.text = user.dob?.age.toString()
+        binding.userAddress.text = user.location?.street?.name
+        binding.userCity.text = user.location?.city
+        binding.userCountry.text = user.location?.country
+        binding.userEmail.text = user.email
+        binding.userPhone.text = user.phone
 
 
     }
